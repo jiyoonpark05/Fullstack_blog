25f3cc3231fad0cefc9b94c02ad6c80c20aec260
jiyoonPark <jiyoonpark05@gmail.com>
2020-09-30 13:55:09 +0900

    make router

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,18 +1,13 @@
 const Router = require('koa-router');
+const postCtrl = require('./posts.ctrl');
+
 const posts = new Router();
 
-const pirntInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
+posts.get('/', postCtrl.list);
+posts.post('/', postCtrl.write);
+posts.get('/:id', postCtrl.read);
+posts.delete('/:id', postCtrl.remove);
+posts.put('/:id', postCtrl.replace);
+posts.patch('/:id', postCtrl.update);
 
-posts.get('/', pirntInfo);
-posts.post('/', pirntInfo);
-posts.get('/:id', pirntInfo);
-posts.delete('/:id', pirntInfo);
-posts.put('/:id', pirntInfo);
-posts.patch('/:id', pirntInfo);
 module.exports = posts;
---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,107 @@
+let postId = 1; //id의 초기값
+
+// post 배열의 초기 데이터
+const posts = [
+  {
+    id: 1,
+    title: 'title',
+    body: 'context',
+  },
+];
+
+// 포스트 작성
+// POST/api/posts
+// {title,body}
+exports.write = (ctx) => {
+  //REST API의 Request bodysms ctx.request.body에서 조회할 수 있다.
+  const { title, body } = ctx.request.body;
+  postId += 1; // 기존 postid값에 1을 더함
+  const post = { id: postId, title, body };
+  posts.push(post);
+  ctx.body = post;
+};
+
+//포스트 목록 조회
+//GET /api / posts
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+
+//특정포트스 조회 GET/api/:id
+exports.read = (ctx) => {
+  const { id } = ctx.params;
+  //주어진 id 값으로 포스트를 찾음
+  //파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나 비교할 p.id값을 문자열로 변경해야
+  const post = post.find((p) => p.id.toString() === id);
+  if (!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  ctx.body = post;
+};
+
+//특정 포스트 제거 DELETE/api/posts/:id
+exports.remove = (ctx) => {
+  const { id } = ctx.params;
+  //해당 id를 가진 포스트가 몇번째 인지 확인
+  const index = post.findIndex((p) => p.id.toString === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  //index 번째 아이템을 제거합니다.
+  posts.splice(index, 1);
+  ctx.status = 204; //no content
+};
+
+//포스트 수정 (교체) PUT/api/posts/:id
+exports.replace = (ctx) => {
+  //포스트 전체 정보를 입력해 데이터를 통째로 교체할 때 사용
+  const { id } = ctx.params;
+  //해당 아이디를 가진 post가 몇 번째인지 확인
+  const index = post.findIndex((p) => p.id.toString() === id);
+  if ((index = -1)) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+
+  // 전체 객체를 덮어 씌움
+  // 따라서 id를 제외한 기존 정보를 ㅏㄴㄹ리고 객체를 새로 만든다
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+//포스트 수정 (특정필드 변경) PATCH/api/posts/:id
+// {title,body}
+exports.update = (ctx) => {
+  //path매서드는 주어진 필드만 교체
+  const { id } = ctx.params;
+  //해당 id를 가진 post가 몇 번 째인지 확인
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if ((index = -1)) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+
+  //기존 값에 정보를 덮어씌운다.
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
