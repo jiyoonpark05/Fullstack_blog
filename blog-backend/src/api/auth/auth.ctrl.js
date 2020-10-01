03a093f33df094c2f9166ca14b6c0f5ea70ca7d5
jiyoonPark <jiyoonpark05@gmail.com>
2020-10-01 23:56:22 +0900

    [feat] generateToken

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -41,6 +41,12 @@ export const register = async (ctx) => {
 
     //응답할 데이터에서 hashedpassword 필드 제거
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -70,6 +76,11 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -25,6 +26,21 @@ UserSchema.methods.serialize = function () {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
+};
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign(
+    //첫번째 파라미터에는 토큰 안에 넣고 싶은 데이터를 넣는다.
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, //두번째 파라미터에는 jwt암호
+    {
+      expiresIn: '7d',
+    },
+  );
+  return token;
 };
 
 const User = mongoose.model('User', UserSchema);
