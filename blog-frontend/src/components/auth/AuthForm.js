a45e09a5770f6d3fc610b5052d2988893b1404fc
jiyoonPark <jiyoonpark05@gmail.com>
2020-10-06 22:55:48 +0900

    [feat] Login & Join

---
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
+
+/*
+회원가입 또는 로그인 폼을 보여준다
+*/
+
+const AuthFormBlock = styled.div`
+  h3 {
+    margin: 0;
+    color: ${palette.gray[8]};
+    margin-bottom: 1rem;
+  }
+`;
+
+/**
+ * 스타일링된 input
+ */
+
+const StyledInput = styled.input`
+font-size : 1rem;
+border:none;
+border-bottom:1rem solid ${palette.gray[5]};
+padding-bottom : 0.5rem;
+outline : none;
+width : 100%
+&:focus {
+    color:$oc-teal-7;
+    border-bottom : 1px solid ${palette.gray[7]};
+}
+&+& {
+    margin-top: 1rem;
+}
+`;
+/**
+ * 폼 하단에 로그인 또는 회원가입 링크
+ */
+
+const Footer = styled.div`
+  margin-top: 2rem;
+  text-align: rigt;
+  a {
+    color: ${palette.gray[6]};
+    text-decoration: underline;
+    &:hover {
+      color: ${palette.gray[9]};
+    }
+  }
+`;
+const AuthForm = () => {
+  return (
+    <AuthFormBlock>
+      <h3>login</h3>
+      <form>
+        <StyledInput autoComplete="username" name="username" placeholder="ID" />
+        <StyledInput
+          autoComplete="new-password"
+          name="password"
+          placeholder="pass"
+          type="password"
+        />
+        <Button>로그인</Button>
+      </form>
+      <Footer>
+        <Link to="/register">회원가입</Link>
+      </Footer>
+    </AuthFormBlock>
+  );
+};
+
+export default AuthForm;
--- blog-frontend/src/pages/Register.js
+++ blog-frontend/src/pages/Register.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 const Register = () => {
-  return <div> Register </div>;
+  return (
+    <AuthTemplate>
+      <AuthForm />
+    </AuthTemplate>
+  );
 };
 
 export default Register;
