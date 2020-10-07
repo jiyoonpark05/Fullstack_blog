33d94a6fa57c7dc7957e5c8bdd4e1b01d113a1b5
jiyoonPark <jiyoonpark05@gmail.com>
2020-10-07 23:45:37 +0900

    [feat] Register function

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,1 +1,44 @@
-import 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({
+    form: auth.register,
+  }));
+
+  // (event Hangler) input changing
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'register',
+        key: name,
+        value,
+      }),
+    );
+  };
+
+  // ( event Handler) registerin form
+  const onSubmit = (e) => {
+    e.prevntDefault();
+  };
+
+  // initialize the form when components renders
+  useEffect(() => {
+    dispatch(initializeForm('register'));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default RegisterForm;
